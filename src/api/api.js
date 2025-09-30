0743831a8faf2fe5546fce6d5e35f7f5ae1f41e3
Emanuele Vitale <emy.2004.vitale@gmail.com>
2025-09-30 16:08:48 +0200

    Collegato frontend al backend su Render

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://horse-backend.onrender.com/api';
+const API_BASE = 'https://horse-backend-hnbs.onrender.com/api';
 
 const headers = (token) => ({
   'Content-Type': 'application/json',
